3b1ee690458f25100130a4ff484a34d388c69465
Nitsan Bracha <nitsi.br@gmail.com>
2015-05-26 21:06:05 +0300

    fill insted of line for the cube

--- Computer Graphics/CG_Ex5/CG_ex5_partial/src/ex5/models/Cube.java
+++ Computer Graphics/CG_Ex5/CG_ex5_partial/src/ex5/models/Cube.java
@@ -9,11 +9,11 @@ public class Cube implements IRenderable {
     public void render(GL gl) {
         gl.glPushMatrix();
 
-        gl.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_LINE);
+//        gl.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_LINE);
+        gl.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_FILL);
 
         gl.glScaled(0.5, 0.5, 0.5);
 
-        gl.glColor3f(1.0f, 1.0f, 0.0f);
         // Page 27 ppt 06
         gl.glBegin(GL.GL_QUADS);
         {
